c8b8705cf686be011c42f6abaff98ae3ca803dd1
EPMA_Castaing <bruder.kraft225@gmail.com>
2018-12-07 13:56:23 -0600

    fix(xes) correctly traverses xes files in full

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const fs = require('fs');
-
-const conditions = require('./util/conditions');
-const position = require('./structures/position');
-const csv = require('./util/csv');
 const Directory = require('./structures/directory');
+const commands = require('./util/convertion');
 
-const BitView = require('bit-buffer').BitView;
 //const conditionRegex = /^\$(.*?) (.*$)/gmi;
 
 // given a top-file, locate valid internal structures and grab the data from them
@@ -15,79 +10,66 @@ const BitView = require('bit-buffer').BitView;
 // .mrc - map_raw_condition version 1
 // .cnf - sem_data-version 1
 
-const topDirectory = new Directory(process.argv[2], {
-	validDir: dir => {
-		return dir.name.endsWith('_QLW');
-	}
-});
+function help() {
+	console.log('--xes or -x\n  Converts the xes files into an output file located in the directory given\n--qlw or -q\n  Converts the qlw files into and output files located in the directory given\n--help or -h\n Provides this output');
+}
 
-let positions = [];
+let options = {
+	topDirectoryUri: '',
+	xes: false,
+	qlw: false,
+	help: false
+};
 
-for (const [dirName, dir] of topDirectory.getDirectories()) {
-	const files = dir.getFiles();
-	const dirs = dir.getDirectories();
-	if (files.has('MAP.cnd') && files.has('MapRawCond.mrc') && files.has('wd_spc_init.cnf')) {
-		const mapCondition = conditions.cndConditionsToMap(fs.readFileSync(`${dir.getUri()}/MAP.cnd`));
-		if (mapCondition.get('sem_data_version') !== '0')
-			console.warn(`Does not output sem_data_version 0. This may break things![${dir.getUri()}]\n`);
-
-		const mapRawCondition = conditions.mrcConditionsToMap(fs.readFileSync(`${dir.getUri()}/MapRawCond.mrc`));
-		if (mapRawCondition.get('map_raw_condition').get('version') !== '1')
-			console.warn(`Does not output map_raw_condition version 1. This may break things![${dir.getUri()}]\n`);
-
-		const wdSpcInit = conditions.cndConditionsToMap(fs.readFileSync(`${dir.getUri()}/wd_spc_init.cnf`));
-		if (wdSpcInit.get('sem_data_version') !== '1')
-			console.warn(`Does not output sem_data_version 1. This may break things![${dir.getUri()}]\n`);
-
-		for (const [posName, pos] of dirs) {
-			if (posName.startsWith('Pos_')) {
-				const posFiles = pos.getFiles();
-				if (posFiles.has('1.xes') && posFiles.has('data001.cnd')) {
-					const positionCondition = conditions.cndConditionsToMap(fs.readFileSync(`${pos.getUri()}/data001.cnd`));
-					if (positionCondition.get('sem_data_version') !== '0')
-						console.warn(`Does not output sem_data_version 0. This may break things![${pos.getUri()}]\n`);
-
-					let positionData = {
-						metadata: position(mapCondition, mapRawCondition, wdSpcInit, positionCondition),
-						probeData: [],
-						probeNoise: []
-					};
-
-                    const BinByteLength = 4*(Number.parseInt(positionData.metadata.get('binYLength'))+0);
-                    const TotalBinByteLength = BinByteLength*positionData.metadata.get('binsY');
-
-					const xesBytes = fs.readFileSync(`${pos.getUri()}/1.xes`, {encoding: null}).buffer;
-					let xesHeader = new BitView(xesBytes, 0, 892);
-					let xesData = new BitView(xesBytes, 892, TotalBinByteLength);
-					let xesNoise = new BitView(xesBytes, TotalBinByteLength + 2, xesBytes.byteLength - (TotalBinByteLength) - 4); // 2 byte offset on each end
-
-					for (let i = 0; i < positionData.metadata.get('binsY'); i++) {
-						let xesProbeData = [];
-						for (let k = 0; k < positionData.metadata.get('binYLength'); k++)
-                            xesProbeData.push(xesData.getUint32((BinByteLength*4*i) + (32*k)));
-                        positionData.probeData.push(xesProbeData);
-                    }
-
-                    for (let i = 0; i < positionData.metadata.get('binsY'); i++) {
-                        let xesProbeNoise = [];
-                    	for (let k = 0; k < positionData.metadata.get('binYLength'); k++)
-                            xesProbeNoise.push(xesNoise.getUint32((BinByteLength*4*i) + (32*k)));
-                        positionData.probeNoise.push(xesProbeNoise);
-                    }
-
-					positions.push(positionData);
-				} else
-					console.warn(`Not processing, missing position files. Make sure data001.cnd and 1.xes are present [${pos.getUri()}]\n`);
-			} else
-				console.warn(`Skipping, identified as not a position. [${pos.getUri()}]\n`);
-		}
-
-	} else {
-		throw {
-			message: `Does not contain required files. Make sure all map condition files are present. [${dir.getUri()}]`,
-			code: 1
-		};
+for (let i = 2; i < process.argv.length; i++) {
+	switch (process.argv[i]) {
+		case '--xes':
+		case '-x':
+			options.xes = true;
+			break;
+		case '--qlw':
+		case '-q':
+			options.qlw = true;
+			break;
+		case '--help':
+		case '-h':
+			options.help = true;
+			break;
+		default:
+			options.topDirectoryUri = process.argv[i];
 	}
 }
 
-csv.writeXesToFile(`${process.argv[2]}/xes_output.csv`, positions);
+if (options.topDirectoryUri === '' && !options.xes && !options.qlw)
+	options.help = true;
+
+if (options.help)
+	help();
+else {
+	try {
+		if (!options.topDirectoryUri)
+			console.error("Please enter a uri of a directory to process, use with no options or -h for help");
+		else {
+			const topDirectory = new Directory(options.topDirectoryUri, {
+				validDir: dir => {
+					return dir.name.endsWith('_QLW');
+				}
+			});
+
+			if (!options.qlw && !options.xes)
+				options.xes = true;
+
+			if (options.xes)
+				commands.xesConvert(topDirectory);
+
+			if (options.qlw)
+				commands.qlwConvert(topDirectory);
+		}
+	} catch(err) {
+		console.error(err);
+	}
+}
+
+
+
+
---
+++ util/convertion.js
@@ -0,0 +1,109 @@
+const fs = require('fs');
+
+const constants = require('./constants');
+
+const conditions = require('./conditions');
+const position = require('../structures/position');
+const csv = require('./csv');
+
+const BitView = require('bit-buffer').BitView;
+
+function qlwConvert(topDirectory) {
+
+}
+
+function xesConvert(topDirectory) {
+	let positions = [];
+
+	for (const [, dir] of topDirectory.getDirectories()) {
+		const files = dir.getFiles();
+		const dirs = dir.getDirectories();
+		if (files.has('MAP.cnd') && files.has('MapRawCond.mrc') && files.has('wd_spc_init.cnf')) {
+			const mapCondition = conditions.cndConditionsToMap(fs.readFileSync(`${dir.getUri()}/MAP.cnd`));
+			if (mapCondition.get('sem_data_version') !== '0')
+				console.warn(`Does not output sem_data_version 0. This may break things![${dir.getUri()}]\n`);
+
+			const mapRawCondition = conditions.mrcConditionsToMap(fs.readFileSync(`${dir.getUri()}/MapRawCond.mrc`));
+			if (mapRawCondition.get('map_raw_condition').get('version') !== '1')
+				console.warn(`Does not output map_raw_condition version 1. This may break things![${dir.getUri()}]\n`);
+
+			const wdSpcInit = conditions.cndConditionsToMap(fs.readFileSync(`${dir.getUri()}/wd_spc_init.cnf`));
+			if (wdSpcInit.get('sem_data_version') !== '1')
+				console.warn(`Does not output sem_data_version 1. This may break things![${dir.getUri()}]\n`);
+
+			for (const [posName, pos] of dirs) {
+				if (posName.startsWith('Pos_')) {
+					const posFiles = pos.getFiles();
+					if (posFiles.has('1.xes') && posFiles.has('data001.cnd')) {
+						const positionCondition = conditions.cndConditionsToMap(fs.readFileSync(`${pos.getUri()}/data001.cnd`));
+						if (positionCondition.get('sem_data_version') !== '0')
+							console.warn(`Does not output sem_data_version 0. This may break things![${pos.getUri()}]\n`);
+
+						let positionData = {
+							metadata: position(mapCondition, mapRawCondition, wdSpcInit, positionCondition),
+							probeData: [],
+							probeNoise: []
+						};
+
+						const BinByteLength = 4 * (Number.parseInt(positionData.metadata.get('binYLength')) + 1);
+						const TotalBinByteLength = BinByteLength * Number.parseInt(positionData.metadata.get('binsY'));
+
+						const xesBytes = fs.readFileSync(`${pos.getUri()}/1.xes`, {encoding: null}).buffer;
+//						let xesHeader = new DataView(xesBytes, 0, 892);
+//						if (xesHeader.getUint32(221 * 4) !== Number.parseInt(positionData.metadata.get('binYLength')) + 1)
+//							throw {
+//								message: 'xes file incorrectly read?',
+//								code: 2
+//							};
+//						let xesData = new DataView(xesBytes, 892, TotalBinByteLength);
+//						let xesNoise = new DataView(xesBytes, TotalBinByteLength + 2, xesBytes.byteLength - TotalBinByteLength - 4); // 2 byte offset on each end
+
+						let xesHeader = new BitView(xesBytes, constants.xes.headerByteOffset, constants.xes.headerByteEnd);
+						if (xesHeader.getUint32(constants.xes.dataCheckOffset) !== Number.parseInt(positionData.metadata.get('binYLength')) + 1)
+							throw {
+								message: 'xes file incorrectly read?',
+								code: 2
+							};
+						let xesData = new BitView(xesBytes, constants.xes.dataByteOffset, TotalBinByteLength);
+						let xesNoise = new BitView(xesBytes, TotalBinByteLength + constants.xes.noiseByteOffset, xesBytes.byteLength - (TotalBinByteLength) - constants.xes.noiseByteEndOffset); // 2 byte offset on each end
+						if (xesNoise.getUint32(constants.xes.noiseCheckOffset) !== Number.parseInt(positionData.metadata.get('binYLength')) + 1)
+							throw {
+								message: 'xes file incorrectly read?',
+								code: 2
+							};
+
+						for (let i = 0; i < positionData.metadata.get('binsY'); i++) {
+							let xesProbeData = [];
+							for (let k = 0; k < positionData.metadata.get('binYLength'); k++)
+								xesProbeData.push(xesData.getUint32((BinByteLength * 8 * i) + (4 * 8 * k))); // Measured in bits
+							positionData.probeData.push(xesProbeData);
+						}
+
+						for (let i = 0; i < positionData.metadata.get('binsY'); i++) {
+							let xesProbeNoise = [];
+							for (let k = 0; k < positionData.metadata.get('binYLength'); k++)
+								xesProbeNoise.push(xesNoise.getUint32((BinByteLength * 8 * i) + (4 * 8 * k) + constants.xes.noiseDataOffset)); // Measured in bits
+							positionData.probeNoise.push(xesProbeNoise);
+						}
+
+						positions.push(positionData);
+					} else
+						console.warn(`Not processing, missing position files. Make sure data001.cnd and 1.xes are present [${pos.getUri()}]\n`);
+				} else
+					console.warn(`Skipping, identified as not a position. [${pos.getUri()}]\n`);
+			}
+
+		} else {
+			throw {
+				message: `Does not contain required files. Make sure all map condition files are present. [${dir.getUri()}]`,
+				code: 1
+			};
+		}
+	}
+
+	csv.writeXesToFile(`${process.argv[2]}/xes_output.csv`, positions);
+}
+module.exports = {
+	xesConvert,
+	qlwConvert
+};
